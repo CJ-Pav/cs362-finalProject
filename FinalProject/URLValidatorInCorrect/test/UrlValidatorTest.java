8a001dc034caa9b1f0d99162f16b1702b420b3e8
CJ-Pav <cpav@live.com>
2018-11-30 07:43:36 -0800

    updates

---
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -0,0 +1,64 @@
+
+import junit.framework.TestCase;
+
+//You can use this as a skeleton for your 3 different test approach
+//It is an optional to use this file, you can generate your own test file(s) to test the target function!
+// Again, it is up to you to use this file or not!
+
+
+public class UrlValidatorTest extends TestCase {
+
+
+    public UrlValidatorTest(String testName) {
+        super(testName);
+    }
+
+    public void testManualTest() {
+        //You can use this function to implement your manual testing
+        UrlValidator urlVal = new UrlValidator(null, null, allowAllSchemes);
+
+        // valid
+        assertTrue(urlVal.isValid("http://www.google.com"));
+        assertTrue(urlVal.isValid("http://www.google.com/"));
+        assertTrue(urlVal.isValid("http://www.facebook.com"));
+        assertTrue(urlVal.isValid("http://www.facebook.com/"));
+
+        // invalid
+        assertTrue(urlVal.isValid(" http:///www.google.com")); // space (mid)
+        assertTrue(urlVal.isValid("http:///www. google.com")); // space (mid)
+        assertTrue(urlVal.isValid("http:///www.google.com ")); // space (mid)
+        assertTrue(urlVal.isValid("#$^&*%http:///www.googlecom")); // symbols (beg)
+        assertTrue(urlVal.isValid("http:///www.google^&*%$#com")); // symbols (mid)
+        assertTrue(urlVal.isValid("http:///www.googlecom^&$#%*/")); // symbols (end)
+        assertTrue(urlVal.isValid("")); //null
+
+
+        UrlValidatorTest("http://www.apache.org/licenses/LICENSE-2.0");
+
+    }
+
+    public void testYourFirstPartition() {
+        //You can use this function to implement your First Partition testing
+
+    }
+
+    public void testYourSecondPartition() {
+        //You can use this function to implement your Second Partition testing
+
+    }
+    //You need to create more test cases for your Partitions if you need to
+
+    public void testIsValid() {
+        //You can use this function for programming based testing
+    	
+    	// loop through multiple symbol options, nest loops/symbols
+    	
+    }
+
+	public static void main(String[] args) {
+		// run man tests
+		testManualTest();
+		
+	}
+
+}
